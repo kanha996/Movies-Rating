b537833e978d404956b0a2ceb71fd9f0d94ea156
Kanhaiya Mishra <knhmishra996@gmail.com>
2022-06-29 00:21:22 +0530

    make it better

--- client/src/component/footer/footer.jsx
+++ client/src/component/footer/footer.jsx
@@ -11,7 +11,7 @@ function Footer() {
             <a href="https://www.linkedin.com/in/kanha996/" target="_blank" rel="noreferrer">
               <img className="github-icon" src="https://img.icons8.com/doodle/48/000000/linkedin-circled.png" alt="linkedin"/>
             </a>
-            <a href="https://www.linkedin.com/in/kanha996/" target="_blank" rel="noreferrer">
+            <a href="https://www.github.com/kanha996/" target="_blank" rel="noreferrer">
             <img className="github-icon" src="https://img.icons8.com/plasticine/100/000000/github.png" alt="github"/>
             </a>
           </div>
