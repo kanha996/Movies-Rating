b4734abc1f411bc75b755ac2d8aa5fc0b2ee1d64
Kanhaiya Mishra <knhmishra996@gmail.com>
2023-05-29 13:43:35 +0530

    Update server.js
    Cookie Fixes

--- server/server.js
+++ server/server.js
@@ -27,6 +27,10 @@ db.connect({ username , password , database })
         genid()  {
           return uuid();
         },
+        cookie: {
+      secure: true,
+      httpOnly: true
+    }
         store: new MongoStore({ client: db.getClient() }),
         secret: "qwertyqwerty12345",
         resave: false,
